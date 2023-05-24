6b4bb4b62880e08eee76f0027534f45bc3668ef1
Fábrica de Software CEET <vcortes@sena.edu.co>
2023-05-24 10:41:23 -0500

    Add Project Identity With ASP .NET 6

---
+++ sena.ceet.adso.WebApplicationMVC03Identity/sena.ceet.adso.WebApplicationMVC03Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sena.ceet.adso.WebApplicationMVC03Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ sena.ceet.adso.WebApplicationMVC03Identity/sena.ceet.adso.WebApplicationMVC03Identity/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace sena.ceet.adso.WebApplicationMVC03Identity.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+    }
+}
